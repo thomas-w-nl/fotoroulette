1e9486c0cc9c7bd70e1900e87a167d405b976a59
Thomas Wiggers <thomas.wiggers@hva.nl>
2017-11-14 21:49:13 +0100

    Format code

--- range_sensor/range_sensor.py
+++ range_sensor/range_sensor.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setmode(GPIO.BCM)
 
 TRIG = 23
@@ -7,8 +8,8 @@ ECHO = 24
 
 print "Distance Measurement In Progress"
 
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
 
 GPIO.output(TRIG, False)
 print "Waiting For Sensor To Settle"
@@ -18,11 +19,11 @@ GPIO.output(TRIG, True)
 time.sleep(0.00001)
 GPIO.output(TRIG, False)
 
-while GPIO.input(ECHO)==0:
-  pulse_start = time.time()
+while GPIO.input(ECHO) == 0:
+    pulse_start = time.time()
 
-while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
+while GPIO.input(ECHO) == 1:
+    pulse_end = time.time()
 
 pulse_duration = pulse_end - pulse_start
 
@@ -30,6 +31,6 @@ distance = pulse_duration * 17150
 
 distance = round(distance, 2)
 
-print "Distance:",distance,"cm"
+print "Distance:", distance, "cm"
 
 GPIO.cleanup()
