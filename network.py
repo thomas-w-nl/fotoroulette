77d25c0e8dac029dc07805d0aa3bbb04d7c9606b
Michel Jonk <michel.jonk@hotmail.com>
2017-11-15 10:42:28 +0100

    Removed class and fixed JSON decoding

--- network.py
+++ network.py
@@ -2,28 +2,21 @@ import requests
 import json
 import os
 
-class NetworkingService:
-    url = 'http://fys.localhost'
+def create_session(image_path):
     auth_key = 'SUPER_GEHEIME_KEY'
+    url = 'http://fys.localhost'
+    files = []
 
-    def create_session(self, image_path):
+    for filename in os.listdir(image_path):
+        if filename.endswith(".jpg") or filename.endswith(".jpeg"):
+            fullpath = os.path.join(image_path, filename)
+            files.append(('image[]', open(fullpath, 'rb')))
 
-        files = []
+    headers = { 'auth': auth_key }
+    r = requests.post(url + '/api/session/upload', files=files, headers=headers)
+    content = json.loads(r.content.decode('latin1'))
 
-        for filename in os.listdir(image_path):
-            if filename.endswith(".jpg") or filename.endswith(".jpeg"):
-                fullpath = os.path.join(image_path, filename)
-                files.append(('image[]', open(fullpath, 'rb')))
-
-        headers = { 'auth': self.auth_key }
-        r = requests.post(self.url + '/api/session/upload', files=files, headers=headers)
-
-        content = json.loads(r.content)
-
-        if content.result == 'success':
-            return content.data.sessionId
-        elif content.result == 'error':
-            return content.reason
-
-network = NetworkingService()
-network.create_session('/home/micheljonk/Pictures/fys/')
+    if content['result'] == 'success':
+        return content['data']['sessionId']
+    elif content['result'] == 'error':
+        return content.reason
