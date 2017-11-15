80aab068529a9dc157abd3f3f7ce40a34691f013
Michel Jonk <michel.jonk@hotmail.com>
2017-11-15 01:23:38 +0100

    fixed upload

--- network.py
+++ network.py
@@ -1,15 +1,24 @@
 import requests
 import json
-
-images_path = "/home/micheljonk/"
+import os
 
 class NetworkingService:
     url = 'http://fys.localhost'
     auth_key = 'SUPER_GEHEIME_KEY'
 
-    def create_session(self, data):
-        headers = {'auth' : self.auth_key, 'Content-Type' : 'application/x-www-form-urlencoded'}
-        r = requests.post(self.url + '/api/session/upload', data=data, headers=headers)
+    def create_session(self, image_path):
+
+        files = []
+
+        for filename in os.listdir(image_path):
+            if filename.endswith(".jpg") or filename.endswith(".jpeg"):
+                fullpath = os.path.join(image_path, filename)
+                files.append(('image[]', open(fullpath, 'rb')))
+
+        headers = {'auth': self.auth_key, 'Content-Type': 'application/x-www-form-urlencoded'}
+        r = requests.post(self.url + '/api/session/upload', files=files)
+        print(r.content)
+        exit()
 
         content = json.loads(r.content)
 
@@ -19,4 +28,4 @@ class NetworkingService:
             return content.reason
 
 network = NetworkingService()
-
+network.create_session('/home/micheljonk/Pictures/fys/')
