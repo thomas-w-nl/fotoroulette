e557bc7aa67a22c4587201d046ea89964f1010be
V.D. van de Beek <valentijn.van.de.beek@hva.nl>
2017-11-15 10:46:08 +0100

    Servo from class to generator

--- servo.py
+++ servo.py
@@ -1,64 +1,38 @@
 import RPi.GPIO as GPIO
 import time
 
-
-class Error(Exception):
-    """Base class for exceptions in this module."""
-    pass
-
-class ServoOutOfBounderyError(Error):
-    """Exception raised for errors in the input.
-
-    Attributes:
-        expression -- input expression in which the error occurred
-        message -- explanation of the error
-    """
-
-    def __init__(self, expression, message):
-        self.expression = expression
-        self.message = message
-
-
-class ServoService:
-    angle = 0
-    max_angle = 120
-    pwm = None
-    status = 'stopped'
-
-    def __init__(self):
+class Servo:
+    def __init__(self, max_angle=120, step=5, delay=0):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(18, GPIO.OUT)
         self.pwm = GPIO.PWM(18, 50)
         self.pwm.start(0)
 
-    def update(self, angle, delay = 0, step = 1):
-        if angle > self.max_angle:
-            raise ServoOutOfBounderyError('', 'Exceeded maximum angle of servo')
+        self.max_angle = 120
+        self.angle = 0
+        self.step = step
+        self.delay = delay
 
-        if delay == 0:
-            duty = self.calculate_angle(angle)
-            self.pwm.ChangeDutyCycle(duty)
-            self.angle = angle
-        else:
-            self.status = "started"
-            for x in range(self.angle, angle, step):
-                time.sleep(delay)
-                duty = self.calculate_angle(x)
-                self.pwm.ChangeDutyCycle(duty)
-                self.angle = angle
-                print(self.status)
-                if self.status == "stopped":
-                    break
-            self.status = "stopped"
+    def __next__(self):
+        if self.angle > self.max_angle:
+            raise StopIteration
 
+        self.angle += self.step
 
+        time.sleep(self.delay)
+        duty = self.angle / 20.0 + 2.5
+        self.ChangeDutyCycle(duty)
+
+        return self.angle
+
+    def __iter__(self):
+        return self
 
 
-    def calculate_angle(self, angle):
-        return float(angle) / 20.0 + 2.5
+if __name__ == "__main__":
+    servo = Servo()
 
-    def stop(self):
-        self.status = "stopped"
-
+    for angle in servo:
+        print(angle)
 
 #TODO: make the whole thing async
