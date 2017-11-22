116f614151fb9c2a3cca3cac4ebe1bdfc0f86054
Thomas Wiggers <thomas.wiggers@hva.nl>
2017-11-22 11:32:39 +0100

    klein optional updateje in camera

--- src/hardware/camera.py
+++ src/hardware/camera.py
@@ -8,13 +8,16 @@ CAMERA_H_ANGLE = 62.2
 
 
 class Camera:
-    def take_picture(self) -> np.array:
+    def take_picture(self, index=0) -> np.array:
         # todo dummy data
 
 
         image_list = listdir("../../img")
 
         pick = random.randrange(0, len(image_list))
+
+        if index != 0:
+            pick = index
 
         frame = cv2.imread("../../img/" + str(image_list[pick]))
 
