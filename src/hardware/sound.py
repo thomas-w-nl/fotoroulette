fd89c97f774e48503275abe4c634e3e197bf20b8
Noedel-Man <Noedel-Man@hotmail.com>
2017-11-28 20:59:47 +0100

    geluid

---
+++ src/hardware/sound.py
@@ -0,0 +1,15 @@
+import vlc
+import time
+from src.common.log import *
+
+love = vlc.MediaPlayer("assets/sound/dingDong.mp3")
+finishHim = vlc.MediaPlayer("assets/sound/finishHim.mp3")
+mortalKombat = vlc.MediaPlayer("assets/sound/mortalKombat.mp3")
+mock = vlc.MediaPlayer("assets/sound/heroes.mp3")
+
+
+if __name__ == "__main__":
+    log.debug("sound running as main")
+    mortalKombat.play()
+    time.sleep(60)
+
