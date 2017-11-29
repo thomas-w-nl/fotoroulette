9b96d5b6d03990ac01e64b6f355d8cc4d1d82156
Noedel-Man <Noedel-Man@hotmail.com>
2017-11-29 16:25:05 +0100

    dinges

--- src/hardware/sound.py
+++ src/hardware/sound.py
@@ -5,7 +5,7 @@ from src.common.log import *
 love = vlc.MediaPlayer("assets/sound/dingDong.mp3")
 finishHim = vlc.MediaPlayer("assets/sound/finishHim.mp3")
 mortalKombat = vlc.MediaPlayer("assets/sound/mortalKombat.mp3")
-mock = vlc.MediaPlayer("assets/sound/heroes.mp3")
+heroes = vlc.MediaPlayer("assets/sound/heroes.mp3")
 
 
 if __name__ == "__main__":
