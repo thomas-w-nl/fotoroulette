02c12cf5f4d048c77bc498797da70e3e89fc8d9f
Thomas Wiggers <thomas.wiggers@hva.nl>
2017-12-15 23:44:09 +0100

    Range sensor gebruikt nu add_event_detect zodat hij de pulse niet mist. Mocht iemand zich er aan wagen om netjes op de 2e callback te wachten ipv de sleep(0.047) dan is dat welkom.

--- src/hardware/range_sensor.py
+++ src/hardware/range_sensor.py
@@ -1,10 +1,14 @@
 import RPi.GPIO as GPIO
 import time
 
-SENSOR_ANGLE = 15
 MEASUREMENTS = 3
 TRIG = 23
 ECHO = 24
+
+GELUIDSSNELHEID = 34300
+
+global_time_start = 0
+global_time_end = 0
 
 
 def _init():
@@ -27,19 +31,16 @@ def get_distance() -> int:
     Meet de afstand met de sensor. Hiervoor moeten Echo op pin 24 en Trig op pin 23 aangsloten zijn
     met weerstanden. De sensor wordt geinitaliseerd bij het inladen van dit bestand.
     Voor naukeurigheid wordt de afstand 3x gemeten.
-    :return: De afstand in cm, -1 bij error
+    :return: De afstand in cm
     """
     distance = 0
     # meet meerdere keren voor de zekerheid
     for i in range(MEASUREMENTS):
         raw = _get_distance_raw()
 
-        retry_counter = 0
         while raw < 4:
-            # corrigeer van onwaarschijnlijke afstand en errors
+            # corrigeer van onwaarschijnlijke afstand
             raw = _get_distance_raw()
-            if retry_counter > 5:
-                return -1
 
         distance += raw
 
@@ -49,37 +50,43 @@ def get_distance() -> int:
 
 def _get_distance_raw() -> int:
     """
-    Meet de afstand met de sensor zonder extra checks, returned -1 bij een error
+    Meet de afstand met de sensor zonder extra checks
     :return: De afstand in cm
     """
+    # begin alvast te letten op de echo pin, wachten tot na de trigger kan er voor zorgen dat we de pulse missen.
+    GPIO.add_event_detect(ECHO, GPIO.BOTH, callback=edge_callback)
+
     # trigger de sensor om te meten
     GPIO.output(TRIG, True)
     time.sleep(0.00001)
     GPIO.output(TRIG, False)
 
-    # de meting loopt van high tot low, de duur van de pulse is de afstand
+    time.sleep(0.047)  # max 8m wachten (8m / 171,50ms^-1)
 
-    channel = GPIO.wait_for_edge(ECHO, GPIO.RISING, timeout=50000)
-    if channel is None:
+    pulse_duration = global_time_end - global_time_start
+    if pulse_duration < 0:
         return -1
-    else:
-        pulse_start = time.time()
 
-    channel = GPIO.wait_for_edge(ECHO, GPIO.FALLING, timeout=50000)
-    if channel is None:
-        return -1
-    else:
-        pulse_end = time.time()
-
-    pulse_duration = pulse_end - pulse_start
-    print("Duration: " + str(pulse_duration))
-
-    distance = pulse_duration * 17150  # magic geluidssnelheid factor
+    distance = (pulse_duration * GELUIDSSNELHEID) / 2  # delen door twee omdat het geluid heen en terug gaat
 
     distance = int(distance)  # centimeter precision is realistischer
 
     return distance
 
 
+def edge_callback(channel):
+    global global_time_start
+    global global_time_end
+
+    if GPIO.input(ECHO):
+        global_time_start = time.time()
+    else:
+        global_time_end = time.time()
+
+
+
 if __name__ == "__main__":
     print(_get_distance_raw())
+
+    GPIO.remove_event_detect(ECHO)
+    GPIO.cleanup()
