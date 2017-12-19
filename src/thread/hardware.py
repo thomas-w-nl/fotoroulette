a8c2795a44ead2b6157081f19bc7adc58e95017b
Noedel-Man <Noedel-Man@hotmail.com>
2017-12-19 18:16:48 +0100

    text server

--- src/thread/hardware.py
+++ src/thread/hardware.py
@@ -1,5 +1,30 @@
-import src.thread.server as server
+import socketserver as socket, os
+from src.common.log import *
+
+# TODO: Deze var moet ergens global staan
+addres = "unix_socket"
+
+class ServerHandeler(socket.BaseRequestHandler):
+    """
+    The RequestHandler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        # log.debug("{} wrote:".format(self.client_address[0]))
+        log.debug(self.data)
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
+
+if os.path.exists(addres):
+    os.remove(addres)
+    open(addres, "w+")
 
 
-with socketserver.UnixStreamServer(server.addres, server.ServerHandeler) as addres:
+with socket.UnixStreamServer(addres, ServerHandeler) as addres:
     addres.serve_forever()
--- src/thread/process.py
+++ src/thread/process.py
@@ -1,5 +1,27 @@
-import src.thread.server as server
+
+import socket
+import cv2
+from src.common.log import *
+
+addres = "unix_socket"
+myString = "hello world"
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+try:
+    sock.connect(addres)
+    sock.send(myString.encode())
+    received = str(sock.recv(1024), "utf-8")
+    log.debug(received)
+except socket.error as msg:
+    log.error("server failed: %s", msg)
 
 
-with socketserver.UnixStreamServer(server.addres, server.ServerHandeler) as addres:
-    addres.send("wazaaa")
+if __name__ == "__main__":
+    log.debug("module running as main!")
+    import src.common.tools as tool
+    img = tool.get_image("img/ardnold.jpg")
+    log.debug(type(img))
+    cv2.imencode('.jpg', img)
+    log.debug(type(img))
+
+    # sock.send()
