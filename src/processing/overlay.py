e8a42719e305a91367284d6f7b66535cfed93cfc
Theo Bouwman <theobouwman98@gmail.com>
2018-01-13 14:16:15 +0100

    overlays werken nu goed jonghe bam bier en tieten

--- src/processing/overlay.py
+++ src/processing/overlay.py
@@ -4,97 +4,71 @@ import cv2
 
 def generate_overlay(game):
     """
-    Generate the overlay with the given parameters
+    Generates the final image with the faces and overlays
 
     Args:
          game: the [Game] object
 
     Returns:
-        returns the created overlay
+        returns the final image as np.array
     """
-    overlay = cv2.imread(game.overlay)
+    # the overlay to place on top of the image with background and faces
+    final_overlay = cv2.imread(game.overlay)
+    final_overlay_height, final_overlay_width, final_overlay_channels = final_overlay.shape
 
-    overlay_height, overlay_width, overlay_channel = overlay.shape
+    # image with game background
+    place_holder_image = np.empty((final_overlay_height, final_overlay_width, 3), np.uint8)
+    if game.background_color is not None:
+        place_holder_image[:, :] = game.background_color
+    elif game.extra_background is not None:
+        place_holder_image = cv2.imread(game.extra_background)
 
-    for i, face in enumerate(game.faces):
-        face_offset = game.offsets[i]
+    for index, face in enumerate(game.faces):
+        face_offset = game.offsets[index]
+        face.image = _resize_fit(face.image, int(final_overlay_width / 2) - face_offset['minus_image_width'],
+                                 final_overlay_height - face_offset['minus_image_width'])
 
-        face.image = _resize_fit(face.image, int(overlay_width / 2) - face_offset['minus_image_width'],
-                                 overlay_height - face_offset['minus_image_width'])
+        face_height, face_width, face_channels = face.image.shape
 
-        overlay = _apply_overlay(i, overlay, face.image, face_offset['offset_x'], face_offset['offset_y'], game)
+        offset_x = face_offset['offset_x']
+        offset_y = face_offset['offset_y']
 
-    return overlay
+        if (offset_x < -100) or (offset_x < -100):
+            raise ValueError('offset more than 100%')
 
+        # negative numbers from -100 to 1 are percentage offsets
+        if offset_y < 0:
+            offset_y_percentage = offset_y * -0.01
+            offset_y = int((final_overlay_height - face_width) * offset_y_percentage)
 
-def _apply_overlay(index, bg, fg, offset_x, offset_y, game):
-    """
-    Adds each face and background to the image
+        if offset_x < 0:
+            offset_x_percentage = offset_x * -0.01
+            offset_x = int((final_overlay_width - face_height) * offset_x_percentage)
 
-    Args:
-        index: the index of the current image in the game faces
-        bg: the image background/ overlay. The is mostly the returned image for the previous call
-        fg: the current face
-        offset_x: the x-axis offset for the face
-        offset_y: the y-axis offset for the face
-        game: the game type itself
+        if (face_height > final_overlay_height) or (face_width > final_overlay_width):
+            raise ValueError('Overlay bigger than background')
 
-    Returns:
-        returns the modified image
-    """
-    rows_fg, cols_fg, channels_fg = fg.shape
-    rows_bg, cols_bg, channels_bg = bg.shape
+        if ((offset_y + face_height) > final_overlay_height) or ((offset_x + face_width) > final_overlay_width):
+            raise ValueError('offset too big')
 
-    # create new image to use as base
-    background = np.empty((rows_bg, cols_bg, 3), np.uint8)
+        roi_height_end = offset_y + face_height
+        roi_width_end = offset_x + face_width
 
-    if game.background_color is not None and index is 0:
-        background[rows_bg - 1, cols_bg - 1] = game.background_color
-    elif game.extra_background is not None and index is 0:
-        background = bg
+        # PLACE FACE
 
-    if (offset_x < -100) or (offset_x < -100):
-        raise ValueError('offset more than 100%')
-
-    # negative numbers from -100 to 1 are percentage offsets
-    if offset_y < 0:
-        offset_y_percentage = offset_y * -0.01
-        offset_y = int((rows_bg - rows_fg) * offset_y_percentage)
-
-    if offset_x < 0:
-        offset_x_percentage = offset_x * -0.01
-        offset_x = int((cols_bg - cols_fg) * offset_x_percentage)
-
-    if (rows_fg > rows_bg) or (cols_fg > cols_bg):
-        raise ValueError('Overlay bigger than background')
-
-    if ((offset_y + rows_fg) > rows_bg) or ((offset_x + cols_fg) > cols_bg):
-        raise ValueError('offset too big')
-
-    roi_rows_end = offset_y + rows_fg
-    roi_cols_end = offset_x + cols_fg
-
-    # if the overlay has transparent parts (on top of the faces)
-    # else, just add the faces
-    if game.on_top:
         # Create mask, remove mask and place overlay on [background]
-        background[offset_y:roi_rows_end, offset_x:roi_cols_end] = fg
+        place_holder_image[offset_y:roi_height_end, offset_x:roi_width_end] = face.image
 
-        gray_fg = cv2.cvtColor(bg, cv2.COLOR_BGR2GRAY)
+        gray_fg = cv2.cvtColor(final_overlay, cv2.COLOR_BGR2GRAY)
         _, mask = cv2.threshold(gray_fg, 0, 255, cv2.THRESH_BINARY)
-
         mask_inv = cv2.bitwise_not(mask)
 
-        over_bg = cv2.bitwise_and(background, background, mask=mask_inv)
-        over_bg = cv2.add(over_bg, bg)
+        over_bg = cv2.bitwise_and(place_holder_image, place_holder_image, mask=mask_inv)
+        over_bg = cv2.add(over_bg, final_overlay)
 
-        background = over_bg
-    else:
-        background = bg
+        place_holder_image = over_bg
 
-        background[offset_y:roi_rows_end, offset_x:roi_cols_end] = fg
-
-    return background
+    return place_holder_image
 
 
 def _resize_fit(image: np.array, max_width: int, max_height: int) -> np.array:
@@ -122,3 +96,4 @@ def _resize_fit(image: np.array, max_width: int, max_height: int) -> np.array:
     res = cv2.resize(image, None, fx=final_scale, fy=final_scale, interpolation=cv2.INTER_CUBIC)
 
     return res
+
