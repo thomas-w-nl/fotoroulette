8520d803a4393f3248c61e8cc7fc12e2379ea089
Thomas Wiggers <thomas.wiggers@hva.nl>
2018-01-17 11:54:06 +0100

    gui gaat niet via systemd

--- install.sh
+++ install.sh
@@ -6,35 +6,40 @@ if [[ $UID != 0 ]]; then
     exit 1
 fi
 
+echo $SUDO_USER
+
 update_service_file () {
 # sed -i '/ExecStart=/c ExecStart=$path_of_executable' $service_file_path
 # Update the executable path in a service file to be copied to the systemd folder
 sed -i "/ExecStart=/c ExecStart=$1" "$2"
+sed -i "/WorkingDirectory=/c WorkingDirectory=$PWD" "$2"
+sed -i "/User=/c User=$SUDO_USER" "$2"
 }
 
+
+python_install="/home/thomas/PycharmProjects/raspberry-pi/python_install/bin/python3"
 services_folder="services"
 
 processing_service="fotoroulette_processing.service"
-python_file_processing="python3 $PWD/processing_main.py"
-update_service_file "$python_file_processing" "$services_folder/$processing_service"
-
-
-gui_service="fotoroulette_gui.service"
-python_file_gui="python3 $PWD/gui_main.py"
-update_service_file "$python_file_gui"  "$services_folder/$gui_service"
+python_file_processing="processing_main.py"
 
 
 hardware_service="fotoroulette_hardware.service"
-python_file_hw="python3 $PWD/start_server.py"
-update_service_file "$python_file_hw"  "$services_folder/$hardware_service"
+python_file_hw="start_server.py HARDWARE"
+
+update_service_file "$python_install $PWD/$python_file_processing" "$services_folder/$processing_service"
+update_service_file "$python_install $PWD/$python_file_hw"  "$services_folder/$hardware_service"
 
 # todo misschien in user space voor startup order? https://wiki.archlinux.org/index.php/Systemd/User#Note_about_X_applications
 
 cp services/fotoroulette* /etc/systemd/system/
 
 systemctl enable $processing_service
-systemctl enable $gui_service
 systemctl enable $hardware_service
+
+
+systemctl restart $processing_service
+systemctl restart $hardware_service
 
 echo Done
 
